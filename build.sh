e042b3c8554058453355863a8418232d143777f1
Andrey Sinitsyn <andrey.sin98@gmail.com>
2024-06-15 22:27:28 +0300

    fix: build

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 cd src
-make bin/undionly.kpxe make bin-x86_64-efi/ipxe.efi EMBED=./boot-0-undionly.ipxe
-make bin-x86_64-efi/ipxe.efi EMBED=./boot-1-ipxe.ipxe
+make bin/undionly.kpxe EMBED=./boot-0-undionly.ipxe
+make bin-x86_64-efi/ipxe.efi EMBED=./boot-0-undionly.ipxe
+#make bin-x86_64-efi/ipxe.efi EMBED=./boot-1-ipxe.ipxe
 cd -
